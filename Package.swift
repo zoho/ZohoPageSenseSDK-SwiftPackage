87fc602843dedb1fe1290db64b1368cf19bf4b42
thiagesh.s <thiagesh.senguttuvan@zohocorp.com>
2024-07-16 15:33:08 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ZohoPageSenseSDK-SwiftPackage",
-            targets: ["ZohoPageSenseSDK-SwiftPackage","ZohoPageSenseSDK"]),
+            targets: ["ZohoPageSenseSDK-SwiftPackage","PageSenseFramework"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -20,8 +20,8 @@ let package = Package(
             name: "ZohoPageSenseSDK-SwiftPackageTests",
             dependencies: ["ZohoPageSenseSDK-SwiftPackage"]),
         .binaryTarget(
-            name: "ZohoPageSenseSDK",
-            path: "Sources/ZohoPageSenseSDK.xcframework"
+            name: "PageSenseFramework",
+            path: "PageSenseFramework.xcframework"
         )
     ]
 )
