2185683619af5e2d105a4a027c8d30cd01bf1f7e
thiagesh.s <thiageshsenguttuvan.s@zohocorp.com>
2026-03-11 19:45:17 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "ZohoPageSenseSDK-SwiftPackage",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v13)
     ],
     products: [
         .library(
@@ -15,8 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ZohoPageSenseSDK-SwiftPackage",
-            url: "https://github.com/zh/PageSenseSDK/releases/download/1.0.0/ZohoPageSenseSDK.xcframework.zip",
-            checksum: "5527446ff003d35ccb252c8682470bbe1c1de5b04e08ed25cb4879e60af0caf9"
+            path: "./Frameworks/ZohoPageSenseFramework.xcframework"
         )
     ]
 )
