840912e396b46b4ca5cd3421e5fe90a3c5d9fda6
thiagesh.s <thiageshsenguttuvan.s@zohocorp.com>
2026-03-11 19:42:01 +0530

    zps

--- Package.swift
+++ Package.swift
@@ -1,28 +1,22 @@
-// swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
     name: "ZohoPageSenseSDK-SwiftPackage",
+    platforms: [
+        .iOS(.v11)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ZohoPageSenseSDK-SwiftPackage",
-            targets: ["ZohoPageSenseSDK-SwiftPackage","ZohoPageSenseSDK"]),
+            targets: ["ZohoPageSenseSDK-SwiftPackage"]
+        )
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "ZohoPageSenseSDK-SwiftPackage"),
-        .testTarget(
-            name: "ZohoPageSenseSDK-SwiftPackageTests",
-            dependencies: ["ZohoPageSenseSDK-SwiftPackage"]),
         .binaryTarget(
-            name: "ZohoPageSenseSDK",
-            url: "https://github.com/zoho/ZohoPageSenseSDK/archive/refs/tags/0.7.34.zip",
-            checksum: "92ab79f75b4f01accf82d2ccfa8ba423b7a71569778e78a76608ed6ba5e2cce6"
+            name: "ZohoPageSenseSDK-SwiftPackage",
+            url: "https://github.com/zh/PageSenseSDK/releases/download/1.0.0/ZohoPageSenseSDK.xcframework.zip",
+            checksum: "5527446ff003d35ccb252c8682470bbe1c1de5b04e08ed25cb4879e60af0caf9"
         )
     ]
 )
