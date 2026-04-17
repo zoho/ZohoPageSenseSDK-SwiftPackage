6f642c63fc13462ca346b7189e138fd742b241cc
Thiagesh <thiageshsenguttuvan.s@zohocorp.com>
2026-04-17 11:32:40 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "ZohoPageSenseSDK-SwiftPackage",
             url: "https://raw.githubusercontent.com/zoho/ZohoPageSenseSDK/3b70fc178b327d7e617dc6138f33f5e032ca8fab/Frameworks/PageSenseFramework.xcframework.zip",
-            checksum: "16edfbd6e66bfe5c314a4b0125ffe4d5af150b855ecd1a3cf538f09a6dd7af49"
+            checksum: "a6dd48cb30673a36bcc1c185863c10db90ff5bfb6b811763ff0d7ea25a1cf527"
         )
     ]
 )
